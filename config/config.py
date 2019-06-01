50b7362f14974c7b6fea430d5082756688bb6c26
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-01 13:07:54 +0530

    adds config file to define your hyperparameters of model

---
+++ config/config.py
@@ -0,0 +1,15 @@
+EXPERIMENT_LABEL = "nasnet_v1"
+TRAIN_PATH = 'train'
+TEST_PATH = 'test'
+
+BASE_IMG_DIR = 'data/'
+GENERATOR_RANDOM_SEED = 42
+PRETRAINED = False
+
+BATCH_SIZE = 32
+IMAGE_DIMS = 224
+
+EPOCHS = 2
+
+BASE_MODEL = 'vgg16'
+METRIC = 'accuracy'
