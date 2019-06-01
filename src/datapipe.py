539108cd7acc3300a68581267b957dd8fb26e0ff
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-01 13:23:39 +0530

    removes load_all_data code which was not needed

--- src/datapipe.py
+++ src/datapipe.py
@@ -10,8 +10,6 @@ def get_all_data_generators():
         train_datagen, val_datagen, test_datagen -- three datagenerators
     """
 
-    train_df, val_df, test_df = load_all_data()
-
     data = ImageDataBunch.from_folder(cfg.BASE_IMG_DIR, train= cfg.TRAIN_PATH, valid_pct= 0.1, test= cfg.TEST_PATH, bs=cfg.BATCH_SIZE,
                                   ds_tfms=get_transforms(), size=cfg.IMAGE_DIMS, num_workers=1)\
                                   .normalize(imagenet_stats)
