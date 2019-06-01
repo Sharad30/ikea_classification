2c8e714f7735c8cda792fb7f4d527b101aa9f19d
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-01 13:12:23 +0530

    adds train script where training pipeline is defined

---
+++ train.py
@@ -0,0 +1,19 @@
+from src.architechture import classifier_pretrained
+from src.datapipe import get_all_data_generators
+from src.monitor import callbacks
+import config.config as cfg
+import os
+
+def do_train():
+    filepath = os.path.join("model", cfg.EXPERIMENT_LABEL+cfg.BASE_MODEL+".h5")
+    data = get_all_data_generator()
+    model = classifier_pretrained(data)
+    
+    learn.fit_one_cycle(cfg.EPOCHS, 1e-3)
+    model.save(filepath)
+
+    return model
+
+
+if __name__ == "__main__":
+    do_train()
