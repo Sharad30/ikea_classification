6b63fee76e16f11e5767a76928bc1bb42314a242
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-02 15:44:43 +0530

    fixes html progress bar output and adds logs callback functionality

--- train.py
+++ train.py
@@ -2,16 +2,21 @@ from src.architecture import classifier_pretrained
 from src.datapipe import get_all_data_generators
 import config.config as cfg
 import os
+from fastai.utils.mod_display import *
+from PIL import ImageFile
+from src.monitor import get_callbacks
 
 def do_train():
-    filepath = os.path.join("model", cfg.EXPERIMENT_LABEL+cfg.BASE_MODEL+".h5")
+    filepath = '../../models/'+cfg.EXPERIMENT_LABEL+'_'+cfg.BASE_MODEL
     data = get_all_data_generators()
-    model = classifier_pretrained(data)
-    
-    learn.fit_one_cycle(cfg.EPOCHS, 1e-3)
-    model.save(filepath)
-
-    return model
+    learn = classifier_pretrained(data)
+    callbacks = get_callbacks(learn)
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    with progress_disabled_ctx(learn) as learn:
+      learn.fit_one_cycle(cfg.EPOCHS, 1e-3, callbacks=callbacks)
+      logs_df = learn.csv_logger.read_logged_file()
+      logs_df.to_csv(f'{cfg.EXPERIMENT_LABEL}_{cfg.BASE_MODEL}.csv')
+      learn.save(filepath)
 
 
 if __name__ == "__main__":
