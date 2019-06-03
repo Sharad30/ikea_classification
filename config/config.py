1bb203df25ec39341fd61e468263529cffaab5b0
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-03 12:32:09 +0530

    includes learning rate hyperparameter in config

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-EXPERIMENT_LABEL = "nasnet_v1"
+EXPERIMENT_LABEL = "vgg1"
 TRAIN_PATH = 'train'
 TEST_PATH = 'test'
 
@@ -6,10 +6,11 @@ BASE_IMG_DIR = 'data/'
 GENERATOR_RANDOM_SEED = 42
 PRETRAINED = False
 
+LEARNING_RATE = 3.98E-04
+
 BATCH_SIZE = 32
 IMAGE_DIMS = 224
 
 EPOCHS = 2
 
 BASE_MODEL = 'vgg16'
-METRIC = 'accuracy'
