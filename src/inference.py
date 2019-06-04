50ba41f58d376421f05d5486bd7cfb51beabd57c
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-04 21:24:04 +0530

    computes test df with image_name along with predictions

--- src/inference.py
+++ src/inference.py
@@ -8,4 +8,12 @@ def predict_test(model):
     ds_type = DatasetType.Test
     predictions = model.get_preds(ds_type)
     predicted_labels = pd.Series(np.argmax(predictions[0], axis=1), name='label')
-    return predicted_labels
+
+    image_name = pd.Series(name='image_name')
+    for path in model.data.test_ds.x.items:
+        image_name = image_name.append(pd.Series(str(path).split('/')[-1]))
+
+    image_df = pd.DataFrame(image_name, columns=['image_name']).reset_index(drop=True)
+    test_df = pd.concat([image_df['image_name'], predicted_labels], axis=1)
+
+    return test_df
