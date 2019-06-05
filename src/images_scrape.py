7a8b904d02b86d06b744b00e3248482d30c9750d
Sharad Shriyan <sharad30.ss@gmail.com>
2019-06-05 14:00:46 +0530

    adds function to create test set

--- src/images_scrape.py
+++ src/images_scrape.py
@@ -2,6 +2,20 @@ import urllib.request
 from bs4 import BeautifulSoup
 import requests
 import json
+import os
+import random
+import shutil
+
+
+def create_test_set():
+    train_path = 'data/train'
+    test_path = 'data/test'
+    product_list = ['bed', 'chair', 'desk', 'kitchen']
+    for product in product_list: 
+        image_path_list = random.sample(os.listdir(f'{train_path}/{product}'), 120)
+        for i, image_path in enumerate(image_path_list): 
+            shutil.move(f'{train_path}/{product}/{image_path}', f'{test_path}/{i}_{product}.jpg')
+
 
 def scrape_images(product_name, start, end):
     """This function will take in single product name and scrape images from ikea website
@@ -15,7 +29,7 @@ def scrape_images(product_name, start, end):
 
     url = f"https://w16804f45.api.esales.apptus.cloud/api/v1/panels/product-search?\
             sessionKey=695f7741-892e-4ff3-3cc2-db634b51be1c&customerKey=54fa9e75-8fdd-4af3-307f-72830c8b9e62&\
-            market=INEN&arg.window_first={start}&arg.window_last={end}&arg.search_phrase={product_name}s&\
+            market=INEN&arg.window_first={start}&arg.window_last={end}&arg.search_phrase={product_name}&\
             arg.sort_by=relevance%20desc&arg.catalog_root=category_catalog_inen%3A%27root%27&\
             arg.catalog_filter=type%3A%27functional%27%20OR%20type%3A%27products%27&\
             arg.nr_catalog_categories=3&arg.locale=en_IN&arg.filter=market%3A%27INEN%27"
@@ -32,13 +46,14 @@ def scrape_images(product_name, start, end):
         imgs = soup.findAll("div", {"class":"image-claim-height"})
         for img in imgs:
             img_url = img.img['src'].split("imgurl=")[0]
-            with open(f'data/images/{product_name}/{i}.jpg','wb') as f:
+            with open(f'data/train/{product_name}/{i}.jpg','wb') as f:
                 f.write(requests.get(img_url).content)
                 print(f"Downloaded {i} {product_name} image successfully")
 
 if __name__ == "__main__":
 
-    # add lighting to the list and remove ending 's' from the url
-    products = ['bed', 'chair', 'desk']
-    for product in products:
-        scrape_images(product, 1, 300)
+    # products = ['kitchen']
+    # for product in products:
+    #     scrape_images(product, 1, 1353)
+
+    create_test_set()
